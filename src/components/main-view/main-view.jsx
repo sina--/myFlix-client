3ee230d077be2b622fe2c2c66d2ab63c99536e77
Sina Wolper <sina@thewolpers.com>
2024-09-26 17:53:12 -0700

    Modify script to pull data from movies API

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,78 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card.jsx";
 import { MovieView } from "../movie-view/movie-view.jsx";
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      "id": 1,
-      "Title": "Commando (1985)",
-      "Description": "A retired special forces colonel goes on a mission to rescue his kidnapped daughter.",
-      "Genre": {
-        "Name": "Action",
-        "Description": "Action films usually include high energy, big-budget physical stunts and chases, possibly with rescues, battles, martial arts, or other violent elements."
-      },
-      "Director": {
-        "Name": "Mark L. Lester",
-        "Bio": "Mark L. Lester is an American film director, screenwriter, and producer. He is known for his action-packed films.",
-        "Birth": "1946",
-        "Death": null
-      },
-      "Featured": false,
-      "Poster": "https://image.tmdb.org/t/p/w1280/ollPAAAgZ7euU8VisfqU3cuXhZ6.jpg"
-    },
-    {
-      "id": 2,
-      "Title": "Die Hard (1988)",
-      "Description": "An NYPD officer must save his wife and several others taken hostage by terrorists during a Christmas party at the Nakatomi Plaza in Los Angeles.",
-      "Genre": {
-        "Name": "Action",
-        "Description": "Action films usually include high energy, big-budget physical stunts and chases, possibly with rescues, battles, martial arts, or other violent elements."
-      },
-      "Director": {
-        "Name": "John McTiernan",
-        "Bio": "John McTiernan is an American filmmaker known for his work in the action genre, including the films 'Predator' and 'Die Hard.'",
-        "Birth": "1951",
-        "Death": null
-      },
-      "Featured": false,
-      "Poster": "https://image.tmdb.org/t/p/w1280/7Bjd8kfmDSOzpmhySpEhkUyK2oH.jpg"
-    },
-    {
-      "id": 3,
-      "Title": "Face/Off (1997)",
-      "Description": "An FBI agent undergoes facial transplant surgery to assume the identity of a terrorist, only for the terrorist to assume the agent's identity.",
-      "Genre": {
-        "Name": "Action",
-        "Description": "Action films usually include high energy, big-budget physical stunts and chases, possibly with rescues, battles, martial arts, or other violent elements."
-      },
-      "Director": {
-        "Name": "John Woo",
-        "Bio": "John Woo is a Hong Kong filmmaker, best known for his highly stylized action films, including 'The Killer' and 'Face/Off.'",
-        "Birth": "1946",
-        "Death": null
-      },
-      "Featured": false,
-      "Poster": "https://image.tmdb.org/t/p/w1280/69Xzn8UdPbVnmqSChKz2RTpoNfB.jpg"
-    },
-    {
-      "id": 4,
-      "Title": "The Last Samurai (2003)",
-      "Description": "An American military advisor embraces the Samurai culture he was hired to destroy after being captured in battle.",
-      "Genre": {
-        "Name": "Action",
-        "Description": "Action films usually include high energy, big-budget physical stunts and chases, possibly with rescues, battles, martial arts, or other violent elements."
-      },
-      "Director": {
-        "Name": "Edward Zwick",
-        "Bio": "Edward Zwick is an American filmmaker and producer. He is known for his epic historical films like 'Glory' and 'The Last Samurai.'",
-        "Birth": "1952",
-        "Death": null
-      },
-      "Featured": false,
-      "Poster": "https://image.tmdb.org/t/p/w1280/lsasOSgYI85EHygtT5SvcxtZVYT.jpg"
-    }
-  ]);
+  const [movies, setMovies] = useState([]);
+    useEffect(() => {
+    fetch("https://sw-movie-flix-5fc48d8b332a.herokuapp.com/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        setMovies(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching movies:", error);
+      });
+  }, []);
 
   const [selectedMovie, setSelectedMovie] = useState(null);
 
