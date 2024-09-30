b35186d910135f4099e3dc454fbc1baa89edf5f7
Sina Wolper <sina@thewolpers.com>
2024-09-29 21:30:30 -0700

    Remove uneeded code

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -11,11 +11,9 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 export const MainView = () => {
   const storedUser = JSON.parse(localStorage.getItem("user"));
   const storedToken = localStorage.getItem("token");
-
   const [user, setUser] = useState(storedUser? storedUser : null);
   const [token, setToken] = useState(storedToken? storedToken : null);
   const [movies, setMovies] = useState([]);
-  const [selectedMovie, setSelectedMovie] = useState(null);
 
   useEffect(() => {
     if (!token) {
