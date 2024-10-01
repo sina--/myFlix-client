f1107e74c70d5d3f379cb9b9700bbef739c34e70
Sina Wolper <sina@thewolpers.com>
2024-09-30 17:08:30 -0700

    Allow user to delete profile from profile view

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,17 +1,117 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Button, Card } from "react-bootstrap";
+import { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export const ProfileView = ({ user }) => {
+  const [username, setUsername] = useState(user.Username);
+  const [password, setPassword] = useState("password");
+  const [email, setEmail] = useState(user.Email);
+  const [birthday, setBirthday] = useState(user.Birthday);
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const data = {
+      Username: username,
+      Password: password,
+      Email: email,
+      Birthday: birthday
+    };
+
+    fetch("https://sw-movie-flix-5fc48d8b332a.herokuapp.com/users", {
+      method: "PUT",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }).then((response) => {
+      if (response.ok) {
+        alert("Success");
+        window.location.reload();
+      } else {
+        alert("Profile update failed");
+      }
+    });
+  };
+  
+  const deregister = (event) => {
+    event.preventDefault();
+
+    const data = {
+      Username: username,
+      Password: password,
+      Email: email,
+      Birthday: birthday
+    };
+
+    fetch(`https://sw-movie-flix-5fc48d8b332a.herokuapp.com/users/${username}/deregister`, {
+      method: "DELETE",
+      headers: { Authorization: `Bearer ${localStorage.getItem('token')}`,
+        "Content-Type": "application/json"
+      },
+    }).then((response) => {
+      if (response.ok) {
+        alert(`${username} successfully deleted`);
+        localStorage.clear();
+        navigate("/");
+      } else {
+        alert("Something went wrong");
+      }
+    });
+  };
+
   return (
-    <div>
-      <div>
-        <span>Username: {user.Username}</span>
-      </div>
-      <Link to={`/`}>
-        <button className="back-button">Back</button>
-      </Link>
-    </div>
+    <>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="formUsername">
+          <Form.Label>Username:</Form.Label>
+          <Form.Control
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+            minLength="3"
+          />
+        </Form.Group>
+        <Form.Group controlId="formPassword">
+          <Form.Label>Password:</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </Form.Group>
+        <Form.Group controlId="formEmail">
+          <Form.Label>Email:</Form.Label>
+          <Form.Control
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </Form.Group>
+        <Form.Group controlId="formDate">
+          <Form.Label>Birthday:</Form.Label>
+          <Form.Control
+            type="date"
+            value={birthday}
+            onChange={(e) => setBirthday(e.target.value)}
+            required
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+      <Button variant="danger" onClick={deregister}>
+        Delete User
+      </Button>
+      <Button onClick={() => navigate("/")}>Back</Button>
+    </>
   );
 };
