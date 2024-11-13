adff1d536f8d3711793d9de4524281a311ff99f9
Sina Wolper <sina@thewolpers.com>
2024-11-13 07:31:49 -0800

    Improve movie view styling

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router";
 import { Link } from "react-router-dom";
 import { FavoriteButton } from "../favorite-button/favorite-button.jsx";
+import "./movie-view.scss"; // Import any additional styles here
 
 export const MovieView = ({ movieData, favorites, toggleFavorite }) => {
   const { movieId } = useParams();
@@ -8,10 +9,31 @@ export const MovieView = ({ movieData, favorites, toggleFavorite }) => {
   const isFav = favorites.some((fav) => fav._id === movie._id);
 
   return (
-    <div>
-      <div>
-        <img src={movie.Poster} className="movie-poster" />
-        <div>
+    <div className="movie-view-container">
+      <div className="movie-view-content">
+        <div className="movie-poster-container">
+          <img
+            src={movie.Poster}
+            className="movie-poster"
+            alt={`${movie.Title} Poster`}
+          />
+        </div>
+        <div className="movie-details">
+          <h2>{movie.Title}</h2>
+          <p>
+            <strong>Description:</strong>
+            <br />
+            {movie.Description}
+          </p>
+          <p>
+            <strong>Director:</strong> {movie.Director.Name}
+          </p>
+          <p>
+            <strong>Genre:</strong> {movie.Genre.Name}
+          </p>
+          <Link to={`/`}>
+            <button className="back-button">Back</button>
+          </Link>
           <FavoriteButton
             isFav={isFav}
             movieId={movie._id}
@@ -19,25 +41,6 @@ export const MovieView = ({ movieData, favorites, toggleFavorite }) => {
           />
         </div>
       </div>
-      <div>
-        <span>Title: {movie.Title}</span>
-      </div>
-      <div>
-        <span>
-          Description:
-          <br />
-          {movie.Description}
-        </span>
-      </div>
-      <div>
-        <span>Director: {movie.Director.Name}</span>
-      </div>
-      <div>
-        <span>Genre: {movie.Genre.Name}</span>
-      </div>
-      <Link to={`/`}>
-        <button className="back-button">Back</button>
-      </Link>
     </div>
   );
 };
